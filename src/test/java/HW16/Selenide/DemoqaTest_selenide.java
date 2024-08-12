9e8ce39c69c721751c9e9b9c53f76b444be6356f
tugcekarapinar <101087196+tugcekarapinar@users.noreply.github.com>
2024-08-12 22:17:07 +0300

    ButtonsPage covered ElementsPage

--- src/test/java/HW16/Selenide/DemoqaTest_selenide.java
+++ src/test/java/HW16/Selenide/DemoqaTest_selenide.java
@@ -8,29 +8,31 @@ import org.testng.annotations.Test;
 
 
 public class DemoqaTest_selenide {
-    private ButtonsPage buttonsPage;
+    private ElementsPage elementsPage;
     private WebTablesPage webTablesPage;
 
     @BeforeClass
     public void setup() {
         WebDriverManager.chromedriver();
         Configuration.timeout = 1000;
-        buttonsPage = new ButtonsPage();
+        elementsPage = new ElementsPage();
         webTablesPage = new WebTablesPage();
     }
 
     @Test(priority = 0)
     public void testClickButton() {
-        buttonsPage.OpenPage();
-        buttonsPage.ClickButton().click();
-        Assert.assertEquals(buttonsPage.DynamicClickMessage().text(), "You have done a dynamic click");
+        elementsPage.OpenPage();
+        elementsPage.Buttons().click();
+        elementsPage.ClickButton().click();
+        Assert.assertEquals(elementsPage.DynamicClickMessage().text(), "You have done a dynamic click");
     }
 
     @Test(priority = 1)
     public void testDoubleClickButton() {
-        buttonsPage.OpenPage();
-        buttonsPage.DoubleClickButton().doubleClick();
-        Assert.assertEquals(buttonsPage.DoubleClickMessage().text(), "You have done a double click");
+        elementsPage.OpenPage();
+        elementsPage.Buttons().click();
+        elementsPage.DoubleClickButton().doubleClick();
+        Assert.assertEquals(elementsPage.DoubleClickMessage().text(), "You have done a double click");
     }
 
 
--- src/test/java/HW16/Selenide/ElementsPage.java
+++ src/test/java/HW16/Selenide/ElementsPage.java
@@ -7,11 +7,15 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
-public class ButtonsPage {
+public class ElementsPage {
 
     public void OpenPage() {
-        open("https://demoqa.com/buttons");
+        open("https://demoqa.com/elements");
         getWebDriver().manage().window().maximize();
+    }
+
+    public SelenideElement Buttons(){
+        return  $(By.xpath("/html/body/div[2]/div/div/div/div[1]/div/div/div[1]/div/ul/li[5]")).scrollTo();
     }
 
     public SelenideElement ClickButton(){
